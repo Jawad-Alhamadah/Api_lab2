4a5692c7759ee8d3c28d2819a897731d08b161ec
vortix2950 <vortix2950@ksu.edu>
2024-09-15 13:27:47 +0300

    rick and morty demo

---
+++ script.js
@@ -0,0 +1,22 @@
+
+let container = document.getElementById("container")
+
+
+fetch("https://rickandmortyapi.com/api/character")
+.then(res => res.json())
+.then (data => {
+    data.results.forEach(element => {
+        let img = document.createElement("img")
+        let name = document.createElement("h4")
+        let div = document.createElement("div")
+        container.appendChild(div)
+        div.appendChild(name)
+        div.appendChild(img)
+      
+        div.classList.add("card")
+        img.setAttribute("src", element.image)
+        name.innerText=element.name
+  
+        
+    });
+})
